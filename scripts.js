cd33c6d47216d85802daea5f4efc827b268040f6
J-Skarbek <john@snarkmediaagency.com>
2022-06-01 15:29:34 -0500

    create the basic variables and functions of the calculator.

---
+++ scripts.js
@@ -0,0 +1,24 @@
+let inputOne = '';
+let inputTwo = '';
+let inputThree = '';
+
+function add(inputOne, inputTwo) {
+    let result = inputOne + inputTwo;
+    return result;
+}
+
+function subtract(inputOne, inputTwo) {
+    let result = inputOne - inputTwo;
+    return result;
+}
+
+function mulitply(inputOne, inputTwo) {
+    let result = inputOne * inputTwo;
+    return result;
+}
+
+function divide(inputOne, inputTwo) {
+    let result = inputOne / inputTwo;
+    return result;
+}
+
