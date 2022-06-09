c1d018bc64f4811b167a29f1b76e69ba27c8a7db
J-Skarbek <john@snarkmediaagency.com>
2022-06-09 16:39:05 -0500

    Work through handling the events.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 let inputOne;
 let inputTwo;
 let inputThree;
+let currentOperation = null;
+let shouldResetScreen = false;
 
 const equalsButton = document.getElementById('operate');
 const clearButton = document.getElementById('ON-C');
@@ -13,10 +15,39 @@ const currentOperationScreen = document.getElementById('currentOperationScreen')
 
 
 window.addEventListener('keydown', handleKeyboardInput);
-equalsButton.addEventListener('click', evaluate);
-clearButton.addEventListener('click', clear);
+// equalsButton.addEventListener('click', evaluate);
+// clearButton.addEventListener('click', clear);
+// decimalButton.addEventListener('click', appendDecimal);
 
-decimalButton.addEventListener('click', appendDecimal);
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+);
+
+functionButtons.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+);
+
+
+function appendNumber(number) {
+    if (currentOperationScreen.textContent === '0' || shouldResetScreen) {
+        resetScreen();
+    };
+    currentOperationScreen.textContent += number;
+}
+
+function resetScreen() {
+    currentOperationScreen.textContent = '';
+    shouldResetScreen = false;
+}
+
+function setOperator(operator) {
+    if (currentOperation !== null) { evaluate()
+        firstOperand = currentOperationScreen.textContent;
+        currentOperation = operator;
+        lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+        shouldResetScreen = true;
+    };
+}
 
 
 const add = (inputOne, inputTwo) => {
@@ -40,8 +71,23 @@ const divide = (inputOne, inputTwo) => {
 }
 
 function handleKeyboardInput(e) {
-
+    if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+    if (e.key === '.') appendDecimal();
+    if (e.key === '=' || e.key === 'Enter') evaluate();
+    // if (e.key === 'Backspace') deleteNumber();
+    // if (e.key === 'Escape') clear();
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+        setOperation(convertOperator(e.key))
+    };
 }
+
+function convertOperator(keyboardOperator) {
+    if (keyboardOperator === '/') return '÷'
+    if (keyboardOperator === '*') return '×'
+    if (keyboardOperator === '-') return '−'
+    if (keyboardOperator === '+') return '+'
+  }
+
 
 
 
