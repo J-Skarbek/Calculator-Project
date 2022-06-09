de5d104361249d30adf45b0cd2c16d3464364866
J-Skarbek <john@snarkmediaagency.com>
2022-06-09 11:22:17 -0500

    Add hover effects to the buttons. Refactor JS functions to arrow functions and cleaned up .js file.

--- scripts.js
+++ scripts.js
@@ -5,30 +5,29 @@ let inputThree;
 const buttons = document.querySelectorAll('.buttons');
 
 
-let additionResult = add();
-let subtractionResult = subtract();
-let mulitplicationResult = multiply();
-let divisionResult = divide();
-
-function add(inputOne, inputTwo) {
+let add = (inputOne, inputTwo) => {
     let result = inputOne + inputTwo;
     return Number(result.toFixed(3));
 }
 
-function subtract(inputOne, inputTwo) {
+let subtract = (inputOne, inputTwo) => {
     let result = inputOne - inputTwo;
     return Number(result.toFixed(3));
 }
 
-function multiply(inputOne, inputTwo) {
+let multiply = (inputOne, inputTwo) => {
     let result = inputOne * inputTwo;
     return Number(result.toFixed(3));
 }
 
-function divide(inputOne, inputTwo) {
+let divide = (inputOne, inputTwo) => {
     let result = inputOne / inputTwo;
     return Number(result.toFixed(3));
 }
+
+
+
+// function calculate() {};
 
 
 
