0de76f5e0e891c59633a36273b45af8c6b88751a
J-Skarbek <john@snarkmediaagency.com>
2022-06-14 13:19:03 -0500

    Add variables for current operation and a flag to reset the screen view. Work through issues with mouse click input.

--- newScripts.js
+++ newScripts.js
@@ -1,15 +1,25 @@
 let inputOne;
 let inputTwo;
+let currentOperation = null;
+let shouldResetScreen = false;
 
 
 const equalsButton = document.getElementById('operate');
 const clearButton = document.getElementById('ON-C');
+const deleteButton = document.getElementById('deleteBtn');
 const decimalButton = document.getElementById('decimal');
 const numberButtons = document.querySelectorAll('.buttons');
 const functionButtons = document.querySelectorAll('.funct-button');
 
 const lastOperationScreen = document.getElementById('lastOperationScreen');
 const currentOperationScreen = document.getElementById('currentOperationScreen');
+
+const testNumber9 = document.getElementById('btn-9');
+const testNumber5 = document.getElementById('btn-5');
+
+// testNumber9.addEventListener('click', () => appendNumber(e.textContent));
+// testNumber5.addEventListener('click', () => appendNumber(e.textContent));
+
 
 // window.addEventListener('keydown', function(e) {
 //     console.log(e);
@@ -22,6 +32,16 @@ window.addEventListener('keydown', handleKeyInput);
 //     if (currentOperationScreen.textContent === '0') {
 //         currentOperationScreen.textContent += number;
 //     }
+// }
+
+
+
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+// function limitInput(string = '' , limit = 9) {
+//     return string.substring(0, limit)
 // }
 
 function appendNumber(number) {
